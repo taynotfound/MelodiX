73b956efff3a7cda0646c31fede459101aa953b6
taynotfound <marztayron@gmail.com>
2024-10-12 21:26:05 +0200

    refactor: Update Search component UI and functionality

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import SearchResults from './SearchResults';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 const Search = ({ setSongs, addToQueue, queue, setCurrentSongIndex, currentSongIndex }) => {
   const [searchTerm, setSearchTerm] = useState('');
@@ -11,7 +13,7 @@ const Search = ({ setSongs, addToQueue, queue, setCurrentSongIndex, currentSongI
       const [title, artist] = searchTerm.split('-').map(s => s.trim());
       const response = await axios.get(`http://toybox.g3v.co.uk/search?title=${title}&artist=${artist}`);
       const results = response.data.data.result ? [response.data.data.result] : [];
-      setSearchResults(results); // Set the search results
+      setSearchResults(results);
     } catch (error) {
       console.error('Error searching songs:', error);
     }
@@ -21,40 +23,40 @@ const Search = ({ setSongs, addToQueue, queue, setCurrentSongIndex, currentSongI
     <div style={{
       marginBottom: '20px',
       display: 'flex',
+      flexDirection: 'column',
       alignItems: 'center',
-      justifyContent: 'center',
-      overflow: 'scroll',
+      justifyContent: 'center'
     }}>
-      <input
-        type="text"
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-        placeholder="Search for songs (title - artist)"
-        style={{
-          width: '80%',
-          padding: '12px',
+      <div style={{ display: 'flex', width: '80%', marginBottom: '10px' }}>
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder="Search for songs (title - artist)"
+          style={{
+            flex: 1,
+            padding: '12px',
+            borderRadius: '5px',
+            border: '1px solid #6DC0D5',
+            marginRight: '8px',
+            backgroundColor: '#2A2D30',
+            color: '#E5F2FF',
+            fontSize: '1rem'
+          }}
+        />
+        <button onClick={searchSongs} style={{
+          padding: '12px 20px',
           borderRadius: '5px',
-          border: '1px solid #4D9CFF',
-          marginRight: '8px',
-          backgroundColor: '#2A2D30',
+          border: 'none',
+          backgroundColor: '#78C091',
           color: '#E5F2FF',
+          cursor: 'pointer',
+          transition: 'background-color 0.3s',
           fontSize: '1rem'
-        }}
-      />
-      <button onClick={searchSongs} style={{
-        padding: '12px 20px',
-        borderRadius: '5px',
-        border: 'none',
-        backgroundColor: '#FF974D',
-        color: '#E5F2FF',
-        cursor: 'pointer',
-        transition: 'background-color 0.3s',
-        fontSize: '1rem'
-      }}>
-        Search
-      </button>
-      <br></br>
-      {/* Render SearchResults component */}
+        }}>
+          <FontAwesomeIcon icon={faSearch} /> Search
+        </button>
+      </div>
       <SearchResults songs={searchResults} addToQueue={addToQueue} queue={queue} setCurrentSongIndex={setCurrentSongIndex} currentSongIndex={currentSongIndex} />
     </div>
   );
